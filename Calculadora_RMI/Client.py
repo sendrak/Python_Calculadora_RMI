31430b6a04f0037e66c3efea7fcbdb7ffdc04e7b
Lucas de Figueiredo Aguiar <lukasfaguiar@gmail.com>
2022-09-20 17:23:01 -0300

    Update IP

--- Calculadora_RMI/Client.py
+++ Calculadora_RMI/Client.py
@@ -6,7 +6,7 @@ now = datetime.datetime.now()
 print('Informações da Conexão: \nData: '+now.strftime('%d-%m-%y')+'\nHora: '+now.strftime('%H:%M:%S'))
 nome = input("Informe seu nome: ").strip()
 print(Client.get_usid(nome))
-n = int(input("Quantas operações você quer fazer, {0}?".format(nome)))
+n = int(input("Quantas operações você quer fazer, {0} ?".format(nome)))
 while n > 0:
     n = n-1
     numero_1 = int(input("\nInforme o primeiro número: "))
@@ -14,15 +14,15 @@ while n > 0:
     print("Escolha a operação desejada: \n" + "1.Adição \n" + "2.Subtração \n" + "3.Multiplicação \n" + "4.Divisão\n" + "5.Raiz Quadrada\n")
     operacao = int(input("Qual a sua escolha: "))
     if operacao == 1:
-        print(Client.adicao(nome, numero_1, numero_2))
+        print(Client.adicao(numero_1, numero_2))
     elif operacao == 2:
-        print(Client.subtracao(nome, numero_1, numero_2))
+        print(Client.subtracao(numero_1, numero_2))
     elif operacao == 3:
-        print(Client.multiplicacao(nome, numero_1, numero_2))
+        print(Client.multiplicacao(numero_1, numero_2))
     elif operacao == 4:
-        print(Client.divisao(nome, numero_1, numero_2))
+        print(Client.divisao(numero_1, numero_2))
     elif operacao == 5:
-        print(Client.raiz_quadrada(nome, numero_1, numero_2))
+        print(Client.raiz_quadrada(numero_1, numero_2))
     else:
         print("Opção inválida")
 
--- Calculadora_RMI/Server.py
+++ Calculadora_RMI/Server.py
@@ -5,6 +5,11 @@ import datetime
 import math
 import timeit
 
+#Importação para detecção do IP
+import socket
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
 now = datetime.datetime.now()
 print('[SERVER]Data: '+now.strftime('%d-%m-%y')+' Hora: '+now.strftime('%H:%M:%S'))
 @Pyro4.expose
@@ -13,40 +18,40 @@ class Server(object):
         return "[SERVER]Hello, {0}.\n" \
             "Your Current User Session is {1}:".format(nome, random.randint(0, 1000))
 
-    def adicao(self, nome, numero_1, numero_2):
+    def adicao(self, numero_1, numero_2):
         inicio = timeit.default_timer()
         resultado = numero_1 + numero_2
         fim = timeit.default_timer()
-        print('[SERVER]:Requisição de adição feita por %s, duração: %fs' % (nome, fim - inicio))
+        print('[SERVER]:Requisição de adição feita por %s, duração: %fs' % (ip_address, fim - inicio))
         return "[SERVER]:{0} + {1} = {2}".format(numero_1, numero_2, resultado)
 
-    def subtracao(self, nome, numero_1, numero_2):
+    def subtracao(self, numero_1, numero_2):
         inicio = timeit.default_timer()
         resultado = numero_1 - numero_2
         fim = timeit.default_timer()
-        print('[SERVER]:Requisição de subtração feita por %s, duração: %fs' % (nome, fim - inicio))
+        print('[SERVER]:Requisição de subtração feita por %s, duração: %fs' % (ip_address, fim - inicio))
         return "[SERVER]:{0} - {1} = {2}".format(numero_1, numero_2, resultado)
 
-    def multiplicacao(self, nome, numero_1, numero_2):
+    def multiplicacao(self, numero_1, numero_2):
         inicio = timeit.default_timer()
         resultado = numero_1 * numero_2
         fim = timeit.default_timer()
-        print('[SERVER]:Requisição de multiplicação feita por %s, duração: %fs' % (nome, fim - inicio))
+        print('[SERVER]:Requisição de multiplicação feita por %s, duração: %fs' % (ip_address, fim - inicio))
         return "[SERVER]:{0} * {1} = {2}".format(numero_1, numero_2, resultado)
 
-    def divisao(self, nome, numero_1, numero_2):
+    def divisao(self, numero_1, numero_2):
         inicio = timeit.default_timer()
         resultado = numero_1 / numero_2
         fim = timeit.default_timer()
-        print('[SERVER]:Requisição de divisão feita por %s, duração: %fs' % (nome, fim - inicio))
+        print('[SERVER]:Requisição de divisão feita por %s, duração: %fs' % (ip_address, fim - inicio))
         return "[SERVER]:{0} / {1} = {2}".format(numero_1, numero_2, resultado)
 
-    def raiz_quadrada(self, nome, numero_1, numero_2):
+    def raiz_quadrada(self, numero_1, numero_2):
         inicio = timeit.default_timer()
         resultado1 = math.sqrt(numero_1)
         resultado2 = math.sqrt(numero_2)
         fim = timeit.default_timer()
-        print('[SERVER]:Requisição de raiz quadrada feita por %s, duração: %fs' % (nome, fim - inicio))
+        print('[SERVER]:Requisição de raiz quadrada feita por %s, duração: %fs' % (ip_address, fim - inicio))
         return "[SERVER]:Raiz de {0}:{1} Raiz de {2}:{3}".format(numero_1, resultado1, numero_2, resultado2)
 
 
